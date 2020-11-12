e703091eff27adc42614d0cc40d8a85a7705b47f
13946592404 <13946592404@163.com>
2020-11-12 11:11:37 +0800

    [add] i18n-support.js to add all .json to i18n.js

---
+++ src/plugins/i18n-support.js
@@ -0,0 +1,11 @@
+const UserFormTextEN = require('../locales/en-US/ns/UserFormText.json');
+const UserFormTextCH = require('../locales/zh-CN/ns/UserFormText.json');
+
+export default {
+  EN: {
+    UserFormText: UserFormTextEN,
+  },
+  CH: {
+    UserFormText: UserFormTextCH,
+  },
+};
--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -1,9 +1,8 @@
 import VueI18n from 'vue-i18n';
 import Vue from 'vue';
-import getLocale from './locale';
 
-const UserFormTextEN = require('../locales/en-US/ns/UserFormText.json');
-const UserFormTextCH = require('../locales/zh-CN/ns/UserFormText.json');
+import getLocale from './locale';
+import i18nSupport from './i18n-support';
 
 Vue.use(VueI18n);
 
@@ -11,12 +10,8 @@ const i18n = new VueI18n({
   locale: getLocale(),
   fallbackLocale: 'ch',
   messages: {
-    en: {
-      UserFormText: UserFormTextEN,
-    },
-    ch: {
-      UserFormText: UserFormTextCH,
-    },
+    en: i18nSupport.EN,
+    ch: i18nSupport.CH,
   },
 });
 
